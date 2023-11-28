e8d3190b3614481f6cfb90cac49599a5e2227b23
Nachowo <ignacio.arias.t@usach.cl>
2023-11-28 18:10:16 -0300

    inicializacion proyecto

---
+++ src/main/java/com/example/sistemainscripcionasignaturas/SistemaInscripcionAsignaturasApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistemainscripcionasignaturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaInscripcionAsignaturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaInscripcionAsignaturasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sistemainscripcionasignaturas/SistemaInscripcionAsignaturasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistemainscripcionasignaturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaInscripcionAsignaturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
