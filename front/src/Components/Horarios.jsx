2de43367489a1902767991e7df3725570789d731
Nachowo <ignacio.arias.t@usach.cl>
2023-12-01 15:08:52 -0300

    Terminado el index, creados los otros 2 componentes

---
+++ front/src/Components/Horarios.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Horarios = () => {
+    return <h1>este es el horario</h1>;
+}
+
+export default Horarios;
---
+++ front/src/Components/Malla.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Malla = () => {
+    return <h1>esta es la malla</h1>;
+}
+
+export default Malla;
